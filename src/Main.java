892d2010f52c0caad46824513a468f077fc71bee
VladimirKing <cheremuha1@gmail.com>
2020-12-27 16:32:18 +0300

    file

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String>tovar=new ArrayList<>();
+
+        try{  Scanner tov=new Scanner(new File("D:\\newZacaz\\srlad.xlsx"));
+        while (tov.hasNext()){
+            tovar.add(tov.nextLine());
+        }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
