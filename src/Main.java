398be02065cf9d679f7d283777109231a090d4f0
VladimirKing <cheremuha1@gmail.com>
2020-12-28 20:41:02 +0300

    nou pull

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,17 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<String>tovar=new ArrayList<>();
 
-        try{  Scanner tov=new Scanner(new File("D:\\newZacaz\\srlad.xlsx"));
-        while (tov.hasNext()){
+        try{  Scanner tov=new Scanner(new File("srlad.xlsx"));
+
+            while (tov.hasNextBoolean()){
+              tov.nextLine();
             tovar.add(tov.nextLine());
         }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        }
+        for (String i:tovar ) {
+            System.out.println(i);
         }
     }
 }
