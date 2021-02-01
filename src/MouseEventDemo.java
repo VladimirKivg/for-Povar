2aaa5415633e82f18e03098fcb2092a7c38863a7
VladimirKing <cheremuha1@gmail.com>
2021-02-01 22:46:28 +0200

    mm

--- src/MouseEventDemo.java
+++ src/MouseEventDemo.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class MouseEventDemo {
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class MouseEventDemo extends Frame implements MouseListener,
+        MouseMotionListener {
+    String msg="";
+    int mouseX=0,mouseY=0;
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
 }
