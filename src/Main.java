e30ffc68cfe8271edb437f0e6b2aeb91df657458
VladimirKing <cheremuha1@gmail.com>
2021-02-02 00:32:48 +0200

    m

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -5,6 +6,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+//muu();
+//        ArreyFile file = new ArreyFile();
+//        for (String s:file.arrayList()){
+//            System.out.println(s);
+//        }
+        MouseEventDemo demo = new MouseEventDemo();
+        demo.setSize(new Dimension(300,300));
+        demo.setTitle("MouseEventDemo якась хуйня трапилась");
+        demo.setVisible(true);
+
+    }
+
+    private static void muu() {
         ArrayList<String> tovar = new ArrayList<>();
         Scanner tov = null;
         String sss = null;
