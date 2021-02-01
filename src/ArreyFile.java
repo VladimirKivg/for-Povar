ac03ece4fd87a9eb73eb6e40548189a01519acb6
VladimirKing <cheremuha1@gmail.com>
2021-02-02 00:32:08 +0200

    m

--- src/ArreyFile.java
+++ src/ArreyFile.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class ArreyFile {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArreyFile {//переробити під звичайний сканер, його принамі розумію
+    public  ArrayList<String> arrayList (){
+        List<String> result =new ArrayList<>();
+      try {
+        result= Files.readAllLines(Paths.get("srlad.csv"));
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+        return (ArrayList) result;
+    }
 }
